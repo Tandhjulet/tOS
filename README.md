f128adfc4bba81e712638ba3deeb97cc181be2d1
Tandhjulet <madsbechmortensen@hotmail.dk>
2025-03-11 20:16:41 +0100

    added tests

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # The T Operating System
 
-A very much work-in-progress OS for learning rust, x86-64 asm and C.
+A very much work-in-progress OS & kernel for learning rust, x86-64 asm and C.
 -> currently 100% skidded :-)
+
+i want: (totally realistic)
+
+- TCP/IP stack
+- own bootloader (that works with UEFI)
+- some sort of shell (?)
+- filesystem maybe (?)
+- actually make the os
+- permission rings (?)
+
+more realistic:
+
+- come up with something myself to write text to the screen (that's not the vga buffer).
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
 #![no_std] // the std won't be available in the os env
 #![no_main] // disable all rust entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::tests::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
+
+    // Conditional compilation:
+    // The test call is only ran when the test compiler flag is set
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info.message().as_str().unwrap_or("Panicked!"));
     loop {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub fn test_runner(tests: &[&dyn Fn()]) {
+        println!("Running {} tests", tests.len());
+        for test in tests {
+            test();
+        }
+    }
+
+    #[test_case]
+    fn trivial_assertion() {
+        print!("trivial assertion... ");
+        assert_eq!(1, 1);
+        println!("[ok]");
+    }
 }
 
 mod vga_buffer;
