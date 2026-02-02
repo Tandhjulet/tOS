9c7de7aef80c2b29a8787942017cd1de788a5c1c
Mads <madsbechmortensen@hotmail.dk>
2026-02-02 22:17:20 +0100

    feat: memory page management!

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,18 @@
 
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use tOS::{memory::active_level_4_table, println};
-use x86_64::{VirtAddr, structures::paging::PageTable};
+use tOS::{memory::BootInfoFrameAllocator, println};
+use x86_64::structures::paging::Page;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use tOS::memory;
+    use x86_64::VirtAddr;
+
     println!("Hello World{}", "!");
 
     tOS::init();
-
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("	L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
-    }
 
     #[cfg(test)]
     test_main();
