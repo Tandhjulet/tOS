878d6cedfefddf82afb235ea645fbab039a8fbdf
Mads <madsbechmortensen@hotmail.dk>
2026-04-01 16:18:04 +0200

    feat: better BAR support and API

--- src/pci/bar.rs
+++ src/pci/bar.rs
@@ -9,240 +9,170 @@ use x86_64::{
 
 use crate::pci::PciDevice;
 
-pub enum AnyBAR {
-    IO(IOBar),
-    Mem(MemBar),
+#[derive(Debug, Clone, Copy)]
+pub struct IoAddr(pub u16);
+
+#[derive(Debug, Clone, Copy)]
+pub enum MemAddr {
+    Bits32(PhysAddr),
+    Bits64(PhysAddr),
 }
 
-impl AnyBAR {
-    pub fn size(&self) -> u32 {
+impl MemAddr {
+    pub fn phys(&self) -> PhysAddr {
         match self {
-            AnyBAR::IO(io) => io.size(),
-            AnyBAR::Mem(mem) => mem.size(),
-        }
-    }
-
-    pub fn from_raw(raw: u32, offset: u8, dev: Arc<Mutex<PciDevice>>) -> Self {
-        let is_io: bool = (raw & 0x1) == 0x1;
-        if is_io {
-            AnyBAR::IO(IOBar::new(raw, offset, dev))
-        } else {
-            AnyBAR::Mem(MemBar::new(raw, offset, dev))
-        }
-    }
-
-    pub unsafe fn write_command(&mut self, offset: u16, val: u32) {
-        unsafe {
-            match self {
-                AnyBAR::IO(io) => io.write_command(offset, val),
-                AnyBAR::Mem(mem) => mem.write_command(offset, val),
-            }
-        }
-    }
-
-    pub unsafe fn read_command(&mut self, offset: u16) -> u32 {
-        unsafe {
-            match self {
-                AnyBAR::IO(io) => io.read_command(offset),
-                AnyBAR::Mem(mem) => mem.read_command(offset),
-            }
+            MemAddr::Bits32(a) | MemAddr::Bits64(a) => *a,
         }
     }
 }
 
-pub trait BAR {
-    type AddrType;
-
-    fn device(&self) -> &Arc<Mutex<PciDevice>>;
-    fn raw(&self) -> u32;
-    fn offset(&self) -> u8;
-
-    fn is_io(&self) -> bool {
-        let is_io: bool = (self.raw() & 0x1) == 0x1;
-        is_io
-    }
-
-    fn is_mem(&self) -> bool {
-        !self.is_io()
-    }
-
-    fn addr(&self) -> Self::AddrType;
-    fn size(&self) -> u32 {
-        let raw = self.raw();
-        let offset = self.offset();
-
-        let device = self.device().lock();
-
-        device.write(offset, 0xFFFF_FFFF);
-        let mask = device.read(offset);
-        device.write(offset, raw);
-
-        drop(device);
-
-        if self.is_io() {
-            let size = !(mask & 0xFFFF_FFFC) + 1;
-            size
-        } else {
-            let size = !(mask & 0xFFFF_FFF0) + 1;
-            size
-        }
-    }
-
-    unsafe fn write_command(&mut self, offset: u16, val: u32);
-    unsafe fn read_command(&mut self, offset: u16) -> u32;
+#[derive(Debug, Clone, Copy)]
+pub enum BarKind {
+    Io { addr: IoAddr },
+    Mem { addr: MemAddr, prefetchable: bool },
 }
 
-pub struct IOBar {
-    raw: u32,
-    offset: u8,
-    dev: Arc<Mutex<PciDevice>>,
+#[derive(Debug)]
+pub struct Bar {
+    kind: BarKind,
+    size: u32,
 
-    port_addr: u16,
-
-    port_base: Port<u32>,
-    port_data: Port<u32>,
-}
-
-pub struct MemBar {
-    raw: u32,
-    dev: Arc<Mutex<PciDevice>>,
     offset: u8,
 
-    virt_addr: VirtAddr,
-}
-
-impl MemBar {
-    fn is_32(raw: u32) -> bool {
-        let mem_type = (raw >> 1) & 0x3;
-        mem_type == 0x0
-    }
-
-    fn is_64(raw: u32) -> bool {
-        !MemBar::is_32(raw)
-    }
-
-    pub fn set_virt_addr(&mut self, addr: VirtAddr) {
-        self.virt_addr = addr;
-    }
-
-    pub fn virt_addr(&self) -> VirtAddr {
-        self.virt_addr
-    }
-
-    pub fn new(raw: u32, offset: u8, dev: Arc<Mutex<PciDevice>>) -> Self {
-        MemBar {
-            raw,
-            dev,
-            virt_addr: VirtAddr::zero(),
-            offset,
-        }
-    }
-}
-
-// Memory Space BAR layout:
-// Bits 31-4						Bit 3			Bits 2-1	Bit 0
-// 16-Byte Aligned Base Address		Prefetchable	Type		Always 0
-impl BAR for MemBar {
-    // FIXME: only supporting 32-bit membar addrs
-    type AddrType = PhysAddr;
-
-    fn addr(&self) -> Self::AddrType {
-        if MemBar::is_64(self.raw) {
-            panic!("64-bit BARs are unsupported!");
-        }
-
-        let bar_low = self.raw & 0xFFFFFFF0;
-        PhysAddr::new(bar_low as u64)
-    }
-
-    unsafe fn write_command(&mut self, offset: u16, val: u32) {
-        if self.virt_addr.is_null() {
-            panic!("Trying to write without assigning virt addr");
-        }
-
-        let addr = self.virt_addr.as_u64() + (offset as u64);
-        let ptr = addr as *mut u32;
-
-        without_interrupts(|| {
-            unsafe { write_volatile(ptr, val) };
-        });
-    }
-
-    unsafe fn read_command(&mut self, offset: u16) -> u32 {
-        if self.virt_addr.is_null() {
-            panic!("Trying to write without assigning virt addr");
-        }
-
-        let addr = self.virt_addr.as_u64() + (offset as u64);
-        unsafe {
-            return read_volatile(addr as *const u32);
-        }
-    }
-
-    fn device(&self) -> &Arc<Mutex<PciDevice>> {
-        &self.dev
-    }
-
-    fn raw(&self) -> u32 {
-        self.raw
-    }
-
-    fn offset(&self) -> u8 {
-        self.offset
-    }
-}
-
-impl IOBar {
-    pub fn addr(raw: u32) -> u16 {
-        (raw & 0xFFFFFFFC) as u16
-    }
-
-    pub fn new(raw: u32, offset: u8, dev: Arc<Mutex<PciDevice>>) -> Self {
-        let base_addr = IOBar::addr(raw);
-        IOBar {
-            raw,
-            dev,
-            port_addr: base_addr,
-            port_base: Port::new(base_addr),
-            port_data: Port::new(base_addr + 4),
-            offset,
-        }
-    }
+    // Only meaningful for Mem Baars
+    virt: Option<VirtAddr>,
 }
 
 // I/O Space BAR layout
 // Bits 31-2						Bit 1		Bit 0
 // 4-Byte Aligned Base Address		Reserved	Always 1
-impl BAR for IOBar {
-    type AddrType = u16;
 
-    fn addr(&self) -> Self::AddrType {
-        self.port_addr
+// Memory Space BAR layout:
+// Bits 31-4						Bit 3			Bits 2-1	Bit 0
+// 16-Byte Aligned Base Address		Prefetchable	Type		Always 0
+impl Bar {
+    pub fn from_cfg(dev: &PciDevice, offset: u8) -> (Self, u8) {
+        let raw = dev.read(offset);
+
+        let (kind, slots_used) = Self::parse_kind(&dev, offset, raw);
+        let size = Self::probe_size(&dev, offset, raw, &kind);
+
+        (
+            Bar {
+                kind,
+                size,
+                offset,
+                virt: None,
+            },
+            slots_used,
+        )
     }
 
-    unsafe fn write_command(&mut self, offset: u16, val: u32) {
-        unsafe {
-            self.port_base.write(offset as u32);
-            self.port_data.write(val);
-        };
-    }
+    fn parse_kind(dev: &PciDevice, offset: u8, raw: u32) -> (BarKind, u8) {
+        if raw & 0x1 != 0 {
+            let addr = IoAddr((raw & 0xFFFF_FFFC) as u16);
+            (BarKind::Io { addr }, 1)
+        } else {
+            let mem_type = (raw >> 1) & 0x3;
+            let prefetchable = (raw >> 3) & 0x1 != 0;
 
-    unsafe fn read_command(&mut self, offset: u16) -> u32 {
-        unsafe {
-            self.port_base.write(offset as u32);
-            self.port_data.read()
+            match mem_type {
+                // 32-bit
+                0b00 => {
+                    let phys = PhysAddr::new((raw & 0xFFFF_FFF0) as u64);
+                    (
+                        BarKind::Mem {
+                            addr: MemAddr::Bits32(phys),
+                            prefetchable,
+                        },
+                        1,
+                    )
+                }
+                // 64-bit
+                0b10 => {
+                    let raw_hi = dev.read(offset + 4);
+                    let phys = PhysAddr::new(((raw_hi as u64) << 32) | (raw & 0xFFFF_FFF0) as u64);
+                    (
+                        BarKind::Mem {
+                            addr: MemAddr::Bits64(phys),
+                            prefetchable,
+                        },
+                        2,
+                    )
+                }
+                other => panic!("Unknown BAR memory type: {:#b}", other),
+            }
         }
     }
 
-    fn device(&self) -> &Arc<Mutex<PciDevice>> {
-        &self.dev
+    fn probe_size(dev: &PciDevice, offset: u8, original: u32, kind: &BarKind) -> u32 {
+        dev.write(offset, 0xFFFF_FFFF);
+        let mask = dev.read(offset);
+        dev.write(offset, original);
+
+        match kind {
+            BarKind::Io { .. } => !(mask & 0xFFFF_FFFC) + 1,
+            BarKind::Mem { .. } => !(mask & 0xFFFF_FFF0) + 1,
+        }
     }
 
-    fn raw(&self) -> u32 {
-        self.raw
+    pub fn kind(&self) -> BarKind {
+        self.kind
     }
 
-    fn offset(&self) -> u8 {
+    pub fn size(&self) -> u32 {
+        self.size
+    }
+
+    pub fn offset(&self) -> u8 {
         self.offset
+    }
+
+    pub fn virt_addr(&self) -> Option<VirtAddr> {
+        self.virt
+    }
+
+    pub fn set_virt_addr(&mut self, addr: VirtAddr) {
+        assert!(
+            matches!(self.kind, BarKind::Mem { .. }),
+            "Cannot set virt_addr for an I/O BAR"
+        );
+        self.virt = Some(addr);
+    }
+
+    pub unsafe fn write32(&mut self, reg_offset: u16, val: u32) {
+        match self.kind {
+            BarKind::Io { addr } => unsafe {
+                let mut port: Port<u32> = Port::new(addr.0 + reg_offset);
+                port.write(val);
+            },
+            BarKind::Mem { .. } => {
+                let virt = self
+                    .virt
+                    .expect("write32 on Mem BAR before virt_addr was set");
+
+                let ptr = (virt.as_u64() + reg_offset as u64) as *mut u32;
+                without_interrupts(|| unsafe {
+                    write_volatile(ptr, val);
+                })
+            }
+        }
+    }
+
+    pub unsafe fn read32(&mut self, reg_offset: u16) -> u32 {
+        match self.kind {
+            BarKind::Io { addr } => unsafe {
+                let mut port: Port<u32> = Port::new(addr.0 + reg_offset);
+                port.read()
+            },
+            BarKind::Mem { .. } => {
+                let virt = self
+                    .virt
+                    .expect("read32 on Mem BAR before virt_addr was set");
+
+                let ptr = (virt.as_u64() + reg_offset as u64) as *const u32;
+                without_interrupts(|| unsafe { read_volatile(ptr) })
+            }
+        }
     }
 }
