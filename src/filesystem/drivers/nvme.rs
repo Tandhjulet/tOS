43df2fd31af17e3ca5e51c70f7676db8140191d0
Mads <madsbechmortensen@hotmail.dk>
2026-04-02 17:24:02 +0200

    feat: map MMIO in FS

--- src/filesystem/drivers/nvme.rs
+++ src/filesystem/drivers/nvme.rs
@@ -1,7 +1,12 @@
 use alloc::sync::Arc;
 use spin::Mutex;
 
-use crate::{filesystem::drivers::StorageDevice, pci::PciDevice, println};
+use crate::{
+    allocator::mmio,
+    filesystem::drivers::StorageDevice,
+    pci::{PciDevice, bar::BarKind},
+    println,
+};
 
 pub struct NVMe {}
 
@@ -11,6 +16,18 @@ impl NVMe {
         let Some(bar0) = PciDevice::get_bar(&binding, 0) else {
             panic!("Could not find BAR0 for NVMe!");
         };
+
+        // TODO: move this into bar mod
+        if let BarKind::Mem { addr, .. } = bar0.kind() {
+            let virt_addr = {
+                let phys_addr = addr.phys();
+
+                let size = bar0.size() as u64;
+                mmio::map_mmio(phys_addr, size)
+            };
+
+            bar0.set_virt_addr(virt_addr);
+        }
 
         println!("bar0: {:?}", bar0);
 
