ea68b51663d5e07ae3a8b2299434863e10658a93
Mads <madsbechmortensen@hotmail.dk>
2026-04-26 11:50:18 +0200

    feat: improved device reg.

--- kernel/src/filesystem/block/mod.rs
+++ kernel/src/filesystem/block/mod.rs
@@ -1,25 +1,65 @@
-use alloc::{boxed::Box, string::String, sync::Arc, vec::Vec};
+use core::fmt::Display;
+
+use alloc::{
+    boxed::Box, collections::btree_map::BTreeMap, format, string::String, sync::Arc, vec::Vec,
+};
 use spin::Mutex;
 
 pub mod nvme;
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct DeviceId(String);
+
+impl DeviceId {
+    pub fn new(id: impl Into<String>) -> Self {
+        Self(id.into())
+    }
+}
+
+impl Display for DeviceId {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "/dev/{}", self.0)
+    }
+}
+
 pub struct BlockDeviceRegistry {
-    devices: Vec<Box<dyn BlockDevice>>,
+    devices: BTreeMap<DeviceId, Box<dyn BlockDevice>>,
+    type_counters: BTreeMap<&'static str, usize>,
 }
 
 impl BlockDeviceRegistry {
     pub fn new() -> Self {
         Self {
-            devices: Vec::new(),
+            devices: BTreeMap::new(),
+            type_counters: BTreeMap::new(),
         }
     }
 
-    pub fn register<T>(&mut self, device: Arc<Mutex<T>>, block_size: u32, block_count: u64)
+    pub fn register<T>(
+        &mut self,
+        type_name: &'static str,
+        device: Arc<Mutex<T>>,
+        block_size: u32,
+        block_count: u64,
+    ) -> DeviceId
     where
         T: StorageDevice + Send + Sync + 'static,
     {
+        let counter = self.type_counters.entry(type_name).or_insert(0);
+        let id = DeviceId::new(format!("{}{}", type_name, counter));
+        *counter += 1;
+
         let descriptor = BlockDescriptor::new(device, block_size, block_count);
-        self.devices.push(Box::new(descriptor));
+        self.devices.insert(id.clone(), Box::new(descriptor));
+        id
+    }
+
+    pub fn get(&self, id: &DeviceId) -> Option<&dyn BlockDevice> {
+        self.devices.get(id).map(|d| d.as_ref())
+    }
+
+    pub fn count(&self) -> usize {
+        self.devices.len()
     }
 }
 
