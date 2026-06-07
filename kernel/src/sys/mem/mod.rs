36d0d579215fed72cdea2ba39010bc1dc1a7fba3
Mads <madsbechmortensen@hotmail.dk>
2026-06-07 19:37:51 +0200

    feat: scaffold new memory structure

--- kernel/src/sys/mem/mod.rs
+++ kernel/src/sys/mem/mod.rs
@@ -3,6 +3,11 @@ use crate::sys::mem::page::{PageSize, Size4KiB};
 pub const FRAME_SIZE: u64 = Size4KiB::SIZE;
 
 pub mod addr;
+pub mod dma;
 pub mod frame;
+pub mod heap;
+pub mod mmio;
 pub mod page;
+pub mod paging;
 pub mod pmm;
+pub mod vmm;
