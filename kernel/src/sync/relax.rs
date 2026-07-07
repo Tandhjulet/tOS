f6c5753e8a7d5a45f334990d9dfeb7cedafc573a
Mads <madsbechmortensen@hotmail.dk>
2026-07-07 23:49:56 +0200

    chore: throwaway unused arg names

--- kernel/src/sync/relax.rs
+++ kernel/src/sync/relax.rs
@@ -13,13 +13,13 @@ impl RelaxStrategy for Spin {
     // For spinning, we can neither wake nor put to sleep.
     // Just hint that we'll do nothing, and cross our fingers
 
-    fn relax(_atomic: &AtomicU32, _expected: u32) {
+    fn relax(_: &AtomicU32, _: u32) {
         core::hint::spin_loop();
     }
 
-    fn notify_one(atomic: &AtomicU32) {}
+    fn notify_one(_: &AtomicU32) {}
 
-    fn notify_all(atomic: &AtomicU32) {}
+    fn notify_all(_: &AtomicU32) {}
 }
 
 // TODO: when threading is properly set up, a parking strategy
