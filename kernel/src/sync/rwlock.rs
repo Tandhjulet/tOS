12e8383d4322f6692941633bf2b7f0a1e94fc97b
Mads <madsbechmortensen@hotmail.dk>
2026-07-08 16:42:46 +0200

    feat: RwLock!

--- kernel/src/sync/rwlock.rs
+++ kernel/src/sync/rwlock.rs
@@ -10,8 +10,9 @@ use crate::sync::{
 };
 
 pub struct RwLock<T, R = Spin> {
-    writer_ref_count: AtomicU32,
+    writer_wake_count: AtomicU32,
     // Every reader is +2, if a writer is waiting +1
+    // u32::MAX if a writer is currently holding the lock
     state: AtomicU32,
     data: UnsafeCell<T>,
 
@@ -21,7 +22,7 @@ pub struct RwLock<T, R = Spin> {
 impl<T, R: RelaxStrategy> RwLock<T, R> {
     pub const fn new(data: T) -> Self {
         Self {
-            writer_ref_count: AtomicU32::new(0),
+            writer_wake_count: AtomicU32::new(0),
             state: AtomicU32::new(0),
             data: UnsafeCell::new(data),
             _relax: PhantomData,
@@ -59,9 +60,51 @@ impl<T, R: RelaxStrategy> RwLock<T, R> {
         }
     }
 
-    pub fn write(&self) -> WriteLockGuard<'_, RwLock<T, R>, T> {}
+    pub fn write(&self) -> WriteLockGuard<'_, RwLock<T, R>, T> {
+        let mut s = self.state.load(Ordering::Relaxed);
+        loop {
+            // try to acquire the lock
+            if s <= 1 {
+                match self
+                    .state
+                    .compare_exchange(s, u32::MAX, Ordering::Acquire, Ordering::Relaxed)
+                {
+                    Ok(_) => {
+                        return WriteLockGuard {
+                            lock: self,
+                            _data: PhantomData,
+                        };
+                    }
+                    Err(e) => {
+                        s = e;
+                        continue;
+                    }
+                }
+            }
 
-    unsafe fn unlock(&self) {}
+            // assure we don't acquire more readers to avoid writer-starvation
+            if s % 2 == 0 {
+                match self
+                    .state
+                    .compare_exchange(s, s + 1, Ordering::Relaxed, Ordering::Relaxed)
+                {
+                    Ok(_) => {}
+                    Err(e) => {
+                        s = e;
+                        continue;
+                    }
+                }
+            }
+
+            // wait, if we can't acquire the lock
+            let w = self.writer_wake_count.load(Ordering::Acquire);
+            s = self.state.load(Ordering::Relaxed);
+            if s >= 2 {
+                R::relax(&self.writer_wake_count, w);
+                s = self.state.load(Ordering::Relaxed);
+            }
+        }
+    }
 }
 
 unsafe impl<T, R: RelaxStrategy> Lock<T> for RwLock<T, R> {
@@ -70,10 +113,19 @@ unsafe impl<T, R: RelaxStrategy> Lock<T> for RwLock<T, R> {
     }
 
     unsafe fn unlock_from_reader(&self) {
-        todo!()
+        // if there are no active readers left AND we have an
+        // awaiting writer, wake it up!
+        if self.state.fetch_sub(2, Ordering::Release) == 3 {
+            self.writer_wake_count.fetch_add(1, Ordering::Release);
+            R::notify_one(&self.writer_wake_count);
+        }
     }
 
     unsafe fn unlock_from_writer(&self) {
-        todo!()
+        self.state.store(0, Ordering::Release);
+        R::notify_all(&self.state);
+
+        self.writer_wake_count.fetch_add(1, Ordering::Release);
+        R::notify_one(&self.writer_wake_count);
     }
 }
